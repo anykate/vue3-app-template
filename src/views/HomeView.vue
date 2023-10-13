a41c8349ba07b224055589bb07ab0cc92c9f69cc
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-10-13 07:43:44 +0530

    Adding pinia and vue-toastification packages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+</script>
+
 <template>
     <div>
         <h1>Home</h1>
+
+        <!-- {{ store.doubleCount }} -->
+
+        <button @click="store.increment">+</button>
+        {{ store.count }}
+        <button @click="store.decrement">-</button>
     </div>
 </template>
